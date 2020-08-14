1776f89c7ed887486b374935ade4b9a0e1930aff
Ahmed <ahmed.alidjinou@hotmail.com>
2020-08-14 14:33:11 +0200

    Add contact form and function

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 console.log('it working');
-
+if(typeof jQuery!=='undefined'){
+    console.log('jQuery Loaded');
+}
+else{
+    console.log('not loaded yet');
+}
 let theme = localStorage.getItem('theme')
 
 if (theme == null) {
@@ -37,3 +42,16 @@ function setTheme(mode){
 	localStorage.setItem('theme', mode)
 
 }
+
+$("#submit-btn").on("click", function() {
+	name = document.getElementsByName("name")[0].value;
+	console.log(document.getElementsByName("message")[0].value);
+    $.ajax({
+        url: "https://formspree.io/mbjzbqvo", 
+        method: "POST",
+        data: {name: name,subject: document.getElementsByName("subject")[0].value, email: document.getElementsByName("email")[0].value, message: document.getElementsByName("message")[0].value },
+        dataType: "json"
+    });
+    alert('Thanks for the email, we\'ll be in touch promptly.');
+    return false;
+});
